dac08c6e43a17117583b1de718ca928a50f2cacd
s.villaponce <s.villaponce@edu.gva.es>
2026-09-24 09:04:56 +0200

    Hecha Actividad2

--- src/main/java/tema02/actividades/Actividad2.java
+++ src/main/java/tema02/actividades/Actividad2.java
@@ -1,4 +1,20 @@
 package tema02.actividades;
 
+import java.util.Scanner;
+
 public class Actividad2 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        int num1 = sc.nextInt();
+        System.out.print("Introduce otro número: ");
+        int num2 = sc.nextInt();
+        if (num2 == 0) {
+            System.out.println("No se puede dividir entre cero.");
+        } else {
+            double resultado = (double) num1 / num2;
+            System.out.println("El resultado de la división es: " + resultado);
+
+        }
+    }
 }
